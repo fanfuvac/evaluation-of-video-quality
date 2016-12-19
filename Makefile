860f45fd3f0b4f4003eb6bba80e116c2cdd0d6ca
Vaclav Fanfule <fanfuvac@star.star.fit.cvut.cz>
2016-12-19 14:01:44 +0100

    modifications in Makefile and adding time measurement

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 SRC=./src/
+OBJ=./obj/
 all: program
 
 program: cudacode.o
-	nvcc -o program -g -G -L/usr/local/cuda/lib64 -lcuda -lpthread  -Xcompiler -fopenmp $(SRC)*.cpp  $(SRC)stvssim.cu.o $(SRC)main.cu.o $(SRC)SSIM.cu.o
+	nvcc  -o program -std=c++11 -L/usr/local/cuda/lib64 -lcuda -lpthread  -Xcompiler -fopenmp  $(SRC)*.cpp  $(OBJ)main.cu.o $(OBJ)SSIM.cu.o  $(OBJ)stvssim.cu.o
 
 cudacode.o:
-	nvcc -g -G -c -arch=sm_20 $(SRC)stvssim.cu -o $(SRC)stvssim.cu.o
-	nvcc -g -G -c -arch=sm_20 $(SRC)main.cu -o $(SRC)main.cu.o
-	nvcc -g -G -c -arch=sm_20 $(SRC)SSIM.cu -o $(SRC)SSIM.cu.o
+	
+	nvcc -ccbin=/usr/bin/gcc-4.8.5 -std=c++11 -g -G -c  -arch=sm_20 $(SRC)main.cu -o $(OBJ)main.cu.o
+	nvcc -ccbin=/usr/bin/gcc-4.8.5 -std=c++11 -g -G -c -arch=sm_20 $(SRC)SSIM.cu -o $(OBJ)SSIM.cu.o
+	nvcc  -ccbin=/usr/bin/gcc-4.8.5 -std=c++11 -c  -arch=sm_20  $(SRC)stvssim.cu -o $(OBJ)stvssim.cu.o 
 
 clean: rm -rf *o program
--- src/main.cu
+++ src/main.cu
@@ -223,6 +223,9 @@ int main(int argc, char ** argv) {
 					FF_PATH = string(argv[i + 1]);
 
 				}
+				else if (string(argv[i]) == string("CUDA")) { //we will use CUDA computation
+					gpu = 1;
+				}
 				
 				else {
 					//cout << "Not enough or invalid arguments, please try again.\n";
@@ -261,7 +264,7 @@ int main(int argc, char ** argv) {
 		frames[i] = frame->frame_count;
 		sum[i] = 0;
 	}
-	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
 	if (gpu == 1) {
 		if (string(type) == string("STVSSIM")) {
 			cout << "stvssim CUDA" << endl;
@@ -284,9 +287,9 @@ int main(int argc, char ** argv) {
 		countMetric(streams, ref, files_count, frame, type, results); //SSIM, PSNR
 		
 	}
-	chrono::high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
 	 
-	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+	chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
 
 	std::cout << "It took me " << time_span.count() << " seconds.";
 	std::cout << std::endl;
@@ -299,7 +302,7 @@ int main(int argc, char ** argv) {
 		cout << i<<"\t";
 		for (int j = 0; j < files_count; j++) {
 			cout <<results[j][i] << "\t";
-			if (isfinite(results[j][i]))
+			if (std::isfinite(results[j][i]))
 				sum[j] += results[j][i];
 			else frames[j]--;
 		}
